72536b200bd4b7a4a69835bc5eed686fd3dfec17
杜哲 <363958265@qq.com>
2017-11-06 10:55:47 +0800

    update

--- PhotoOpt/PhotoOpt/Views/PhotoBottomView.swift
+++ PhotoOpt/PhotoOpt/Views/PhotoBottomView.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class PhotoBottomView: UIView {
   
+  let previewBtn: UIButton = {
+    let btn = UIButton()
+    btn.titleLabel?.font = UIFont.systemFont(ofSize: 15)
+    btn.setTitle("预览", for: .normal)
+    btn.backgroundColor = UIColor.blue
+    return btn
+  }()
+  
+  let commitBtn: UIButton = {
+    let btn = UIButton()
+    btn.titleLabel?.font = UIFont.systemFont(ofSize: 15)
+    btn.setTitle("完成", for: .normal)
+    btn.backgroundColor = UIColor.blue
+    return btn
+  }()
+  
   override init(frame: CGRect) {
     super.init(frame: frame)
     prepareView()
@@ -22,5 +38,7 @@ class PhotoBottomView: UIView {
   
   func prepareView(){
     self.backgroundColor = UIColor.white
+    addSubview(previewBtn)
+    addSubview(commitBtn)
   }
 }
